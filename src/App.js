0e9f27fc21e0ddf3231c6682c81f41dcc0aa8b32
EIlif Johansen <eilifjohansen@outlook.com>
2021-11-28 14:34:00 +0100

    Added basic functionalities

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+/*global chrome*/
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [siteUrl, setSiteUrl] = useState("");
+  const [siteLogo, setSiteLogo] = useState("");
+  const [siteTitle, setSiteTitle] = useState("");
+  const [siteHTML, setSiteHTML] = useState("");
+
+  /**
+   * Get current URL
+   */
+  useEffect(() => {
+    const queryInfo = { active: true, lastFocusedWindow: true };
+
+    
+    // Get HTML
+    chrome.runtime.onMessage.addListener(function(request, sender) {
+      if (request.action == "getSource") {
+        const message = request.source;
+        setSiteHTML(message);
+        console.log(message.match(/<p>(.*?)<\/p>/g));
+      }
+    });            
+    chrome.tabs.executeScript(null, {
+      file: "getPagesSource.js"
+    }, function() {
+      // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+      if (chrome.runtime.lastError) {
+        const message = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+        setSiteHTML(message);
+      }
+    });
+
+    // Get tab info
+    chrome.tabs &&
+      chrome.tabs.query(queryInfo, (activeTab) => {
+        console.log(activeTab)
+
+        const siteUrl = activeTab[0].url;
+        const siteLogo = activeTab[0].favIconUrl;
+        const siteTitle = activeTab[0].title;
+        
+        setSiteTitle(siteTitle);
+        setSiteUrl(siteUrl);
+        setSiteLogo(siteLogo);
+  
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={siteLogo} className="App-logo" alt="logo" />
+        <div className="site-info" >
+          <p className="site-info-item">{siteTitle}</p>
+          <p className="site-info-item">{siteUrl}</p>
+         
+          <div className="site-info-html">
+          {siteHTML}
+          </div>
+
+        </div>
       </header>
     </div>
   );
